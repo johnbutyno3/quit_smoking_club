503e97407381435dd04c075bd0f5ea2efef844bd
johnyan <johnbutyno3@gmail.com>
2026-08-03 16:39:10 +0800

    feat: add stories content foundation

---
+++ lib/models/story/story_item.dart
@@ -0,0 +1,19 @@
+class StoryItem {
+  final String id;
+  final String title;
+  final String summary;
+  final String category;
+  final String language;
+  final String content;
+  final String sourceUrl;
+
+  const StoryItem({
+    required this.id,
+    required this.title,
+    required this.summary,
+    required this.category,
+    required this.language,
+    required this.content,
+    required this.sourceUrl,
+  });
+}
