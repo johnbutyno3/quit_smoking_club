c8a5a54abebbe7845e5a0b42fab8e0dd421f8a8f
johnbutyno3 <johnbutyno3@gmail.com>
2026-08-10 12:56:20 +0800

    refactor: add reading download usecase

---
+++ lib/usecases/reading/download_reading_book_usecase.dart
@@ -0,0 +1,13 @@
+import '../../models/reading_book.dart';
+import '../../repositories/reading_repository.dart';
+
+class DownloadReadingBookUseCase {
+  DownloadReadingBookUseCase({ReadingRepository? repository})
+    : _repository = repository ?? ReadingRepository();
+
+  final ReadingRepository _repository;
+
+  Future<ReadingDownloadResult> execute(ReadingBook book) {
+    return _repository.downloadBook(book);
+  }
+}
