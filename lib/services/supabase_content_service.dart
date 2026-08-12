ef5d5425932e95eb10fdb985c1b94a5a8cfb1647
johnbutyno3 <johnbutyno3@gmail.com>
2026-08-12 15:11:46 +0800

    refactor: inject Supabase client into content service

--- lib/services/supabase_content_service.dart
+++ lib/services/supabase_content_service.dart
@@ -1,13 +1,20 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'supabase_service.dart';
 
 class SupabaseContentService {
+  SupabaseContentService({SupabaseClient? client})
+    : _client = client ?? SupabaseService.client;
+
+  final SupabaseClient _client;
+
   static const String _table = 'content_items';
 
-  static Future<List<Map<String, dynamic>>> getContents({
+  Future<List<Map<String, dynamic>>> getContents({
     String? language,
     String? category,
   }) async {
-    var query = SupabaseService.client.from(_table).select();
+    var query = _client.from(_table).select();
 
     if (language != null && language.isNotEmpty) {
       query = query.eq('language', language);
@@ -22,29 +29,23 @@ class SupabaseContentService {
     return List<Map<String, dynamic>>.from(response);
   }
 
-  static Future<void> createContent(Map<String, dynamic> data) async {
-    await SupabaseService.client.from(_table).insert(data);
+  Future<void> createContent(Map<String, dynamic> data) async {
+    await _client.from(_table).insert(data);
   }
 
-  static Future<void> updateContent(
+  Future<void> updateContent(
     String uniqueId,
     Map<String, dynamic> data,
   ) async {
-    await SupabaseService.client
-        .from(_table)
-        .update(data)
-        .eq('unique_id', uniqueId);
+    await _client.from(_table).update(data).eq('unique_id', uniqueId);
   }
 
-  static Future<void> deleteContent(String uniqueId) async {
-    await SupabaseService.client
-        .from(_table)
-        .delete()
-        .eq('unique_id', uniqueId);
+  Future<void> deleteContent(String uniqueId) async {
+    await _client.from(_table).delete().eq('unique_id', uniqueId);
   }
 
-  static Future<Map<String, dynamic>?> getContentById(String uniqueId) async {
-    final response = await SupabaseService.client
+  Future<Map<String, dynamic>?> getContentById(String uniqueId) async {
+    final response = await _client
         .from(_table)
         .select()
         .eq('unique_id', uniqueId)
