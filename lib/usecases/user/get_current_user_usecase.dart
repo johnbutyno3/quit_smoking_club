d29da7baee4e37b29f7044322e91041d139fa96c
johnbutyno3 <johnbutyno3@gmail.com>
2026-08-12 13:47:19 +0800

    fix(forum): resolve current user nickname from profile cache

--- lib/usecases/user/get_current_user_usecase.dart
+++ lib/usecases/user/get_current_user_usecase.dart
@@ -1,7 +1,12 @@
+import '../../services/storage_service.dart';
 import 'user_facade_usecase.dart';
 
 class GetCurrentUserUseCase {
   String? executeUid() {
     return UserFacadeUseCase.currentUid;
+  }
+
+  Future<String?> executeName() {
+    return StorageService.getUserName();
   }
 }
