3d6219b798fb46a3279142d167227450c81c5e97
johnbutyno3 <johnbutyno3@gmail.com>
2026-08-13 12:39:16 +0800

    fix(forum): refund coins when post creation fails

--- lib/usecases/forum/create_forum_post_usecase.dart
+++ lib/usecases/forum/create_forum_post_usecase.dart
@@ -1,15 +1,18 @@
 import '../../models/forum_post.dart';
+import '../../repositories/coin/coin_repository.dart';
 import '../../repositories/forum_repository.dart';
 import '../coin/spend_coin_usecase.dart';
 
 class CreateForumPostUseCase {
   final ForumRepository forumRepository;
   final SpendCoinUseCase spendCoinUseCase;
+  final CoinRepository coinRepository;
 
   CreateForumPostUseCase({
     required this.forumRepository,
     required this.spendCoinUseCase,
-  });
+    CoinRepository? coinRepository,
+  }) : coinRepository = coinRepository ?? CoinRepository();
 
   Future<void> execute({required ForumPost post, int cost = 30}) async {
     if (post.content.trim().isEmpty) {
@@ -17,11 +20,15 @@ class CreateForumPostUseCase {
     }
 
     final spent = await spendCoinUseCase.execute(cost, 'forum_create_post');
-
     if (!spent) {
       throw Exception('insufficient_coin');
     }
 
-    await forumRepository.addPost(post);
+    try {
+      await forumRepository.addPost(post);
+    } catch (_) {
+      await coinRepository.addCoin(cost, 'forum_create_post_refund');
+      rethrow;
+    }
   }
 }
